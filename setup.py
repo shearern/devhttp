a2b9e9a9c89cdae443edfb8afa0cacbd93406352
Nathan Shearer <shearern@gmail.com>
2018-11-27 22:33:32 -0800

    Improved setup.py documentation for next PyPI upload

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup
 
-setup(name='devhttp',
-      version='0.0.1', # version string
-      description='Development HTTP Server Library',
-      url='https://github.com/shearern/devhttp',
-      author='Nathan Shearer',
-      author_email='shearern@gmail.com',
-      license='MIT',
-      packages=['devhttp', 'devhttp.endpoints'],
-      classifiers=[
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='devhttp',
+    version='0.0.1', # version string
+    description='Development HTTP Server Library',
+    long_description=readme(),
+    url='https://github.com/shearern/devhttp',
+    author='Nathan Shearer',
+    author_email='shearern@gmail.com',
+    license='MIT',
+    packages=['devhttp', 'devhttp.endpoints'],
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
-      ],
-      install_requires=[
-          'jinja2',
-      ],
-      )
+    ],
+    install_requires=[
+        'jinja2',
+        'markdown',
+    ],
+    include_package_data=True,
+    zip_safe=False,  # True?
+    )
